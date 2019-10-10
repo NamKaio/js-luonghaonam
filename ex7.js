20a13a071c9d6c28471b4b1c6169d77ffdab8999
NamKaio <luonghaonam99@gmail.com>
2019-10-10 09:16:53 +0700

    buoi3

---
+++ ex7.js
@@ -0,0 +1,14 @@
+const numbers = [1, 3, 5, 7, 9];
+
+[num1, , , num2] = numbers;
+
+console.log(num1, num2);
+
+const person = {
+    name: "teo",
+    age: 13,
+};
+
+const {age} = person;
+
+console.log(age);
